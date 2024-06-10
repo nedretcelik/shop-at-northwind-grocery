8b6d54a1784c521c906d433d0cd3e2f6c2b7916e
nedretcelik <nedretcelik97@gmail.com>
2024-06-10 02:43:32 -0400

    display products according to category

--- scripts/products.js
+++ scripts/products.js
@@ -5,7 +5,7 @@ window.onload = () => {
   const selectOption = document.querySelector("#selectOption");
   const categoryList = document.querySelector("#categoryList");
 
-  function loadProducts() {
+  function loadCategory() {
     productsDetailCard.innerText = "";
     if (selectOption.value == "2") {
       categoryList.style.display = "none";
@@ -53,7 +53,7 @@ window.onload = () => {
         .then((response) => response.json())
         .then((categories) => {
           for (const category of categories) {
-            console.log(category)
+            console.log(category);
             const option = new Option("option");
             option.value = category.id;
             option.innerText = category.name;
@@ -64,5 +64,48 @@ window.onload = () => {
     }
   }
 
-  selectOption.onchange = loadProducts;
+  function loadProduct() {
+    fetch("http://localhost:4000/products")
+      .then((response) => response.json())
+      .then((products) => {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        productsDetailCard.appendChild(row);
+        for (const product of products) {
+          if (categoryList.value == product.categoryId) {
+            console.log(product);
+            const col = document.createElement("div");
+            col.classList.add("col-md-3", "mb-3");
+            row.appendChild(col);
+
+            const card = document.createElement("div");
+            card.classList.add("card", "text-center", "h-100");
+            col.appendChild(card);
+
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+            card.appendChild(cardBody);
+
+            const idH5 = document.createElement("h5");
+            idH5.innerText = "Product Id: " + product.id;
+            const nameH6 = document.createElement("h6");
+            nameH6.innerText = "Product Name: " + product.name;
+
+            const descriptionH6 = document.createElement("h6");
+            descriptionH6.innerText = "$" + product.unitPrice;
+            let anchor = document.createElement("a");
+            anchor.href = `details.html?productid=${product.id}`;
+            anchor.text = "See details";
+
+            cardBody.appendChild(idH5);
+            cardBody.appendChild(descriptionH6);
+            cardBody.appendChild(nameH6);
+            cardBody.appendChild(anchor);
+          }
+        }
+      });
+  }
+
+  selectOption.onchange = loadCategory;
+  categoryList.onchange = loadProduct;
 };
